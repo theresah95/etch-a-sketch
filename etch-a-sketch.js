022162e6a8ad435b74f65599e9a5c278558f659a
TheresaH <44990136+theresah95@users.noreply.github.com>
2023-07-04 10:38:31 -0700

    createRow functionality

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,14 +1,25 @@
-function createRow () {
-    //Create Row
+function createRow() {
+
+    //Create Row Container
     const row = document.createElement('div');
-    //Edit Row
     row.style.display = "flex";
-    row.style.alignItems = "stretch";
-    //Create Grid Boxes
-    const boxes = document.createElement('div');
-    //Add Boxes to Row
-    row.appendChild(boxes);
-    //Add Row to Container
+    row.style.justifyContent = "space-evenly";
+    row.style.border = "5px red solid";
+
+    for(i=0; i < 17; i++){
+
+    //Create Grid Boxes for Row
+    const box = document.createElement('div');
+    box.style.border = "5px green solid";
+    box.style.margin = "5px";
+
+    //Append Boxes to Row
+    row.appendChild(box);
+
+    }
+    //Append Row to Container
     const container = document.getElementById('container');
     container.appendChild(row);
-};
+}
+
+createRow();
