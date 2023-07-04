ff1ad67fda97925ee55afa8cf27e12d99346d84c
TheresaH <44990136+theresah95@users.noreply.github.com>
2023-07-04 11:02:52 -0700

    prints a row of squares

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -4,19 +4,18 @@ function createRow() {
     const row = document.createElement('div');
     row.style.display = "flex";
     row.style.justifyContent = "space-evenly";
-    row.style.border = "5px red solid";
 
-    for(i=0; i < 17; i++){
+    for(i=0; i < 16; i++){
+        //Create Grid Boxes for Row
+        const box = document.createElement('div');
+        box.style.border = "1px black solid";
+        box.style.height = "50px";
+        box.style.width = "50px";
 
-    //Create Grid Boxes for Row
-    const box = document.createElement('div');
-    box.style.border = "5px green solid";
-    box.style.margin = "5px";
-
-    //Append Boxes to Row
-    row.appendChild(box);
-
+        //Append Boxes to Row
+        row.appendChild(box);
     }
+
     //Append Row to Container
     const container = document.getElementById('container');
     container.appendChild(row);
