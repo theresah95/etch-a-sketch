f16f249f5e9946da33c752c78d1cf410d9efec64
TheresaH <44990136+theresah95@users.noreply.github.com>
2023-07-04 11:14:32 -0700

    createGrid makes grid of 16x16 boxes

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -3,12 +3,13 @@ function createRow() {
     //Create Row Container
     const row = document.createElement('div');
     row.style.display = "flex";
-    row.style.justifyContent = "space-evenly";
+    row.style.justifyContent = "center";
 
     for(i=0; i < 16; i++){
         //Create Grid Boxes for Row
         const box = document.createElement('div');
         box.style.border = "1px black solid";
+        box.style.backgroundColor = "white";
         box.style.height = "50px";
         box.style.width = "50px";
 
@@ -21,4 +22,15 @@ function createRow() {
     container.appendChild(row);
 }
 
-createRow();
+function createGrid (){
+
+    count = 0;
+
+    //Creates 16 rows
+    while (count != 16){
+        createRow();
+        count++;
+    }
+};
+
+createGrid();
